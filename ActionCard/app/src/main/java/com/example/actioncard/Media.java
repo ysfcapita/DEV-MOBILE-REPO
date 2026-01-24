f71df959af92320dd63e02105863d6e36a93bfb9
youssef belghiti <ysfbel7@gmail.com>
2026-01-24 02:20:14 +0100

    adding App_Media (Action Card)

---
+++ ActionCard/app/src/main/java/com/example/actioncard/Media.java
@@ -0,0 +1,55 @@
+package com.example.actioncard;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Media extends AppCompatActivity {
+
+    ImageView backimg, gobackimg, playimg, nextimg;
+    VideoView videoView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        backimg = findViewById(R.id.backimg);
+
+        videoView1 = findViewById(R.id.videoView1);
+
+
+
+        backimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        videoView1.setVideoPath("android.resource://"+ getPackageName() + "/" + R.raw.avion);
+        MediaController controller = new MediaController(this);
+        videoView1.setMediaController(controller);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
